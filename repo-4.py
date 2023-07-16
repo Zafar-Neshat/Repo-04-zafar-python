cb3baa4b83b2ed5d0a7e49d0a39f5502b6a235f6
zafar neshat <zafarnishat1381@gmail.com>
2023-07-16 22:40:52 +0430

    trannig 04 for making repo

---
+++ repo-4.py
@@ -0,0 +1,9 @@
+hours_in_day = 24
+minutes_in_hour = 60
+seconds_in_minute = 60
+days_in_year = 365
+
+seconds_in_year = hours_in_day * minutes_in_hour * seconds_in_minute * days_in_year
+weeks_in_year = days_in_year // 7
+
+print(weeks_in_year)
